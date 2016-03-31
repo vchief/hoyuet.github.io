4c6ead781ab62c142b0bc19d6a609a1f0f067f3a
hoyuet <201568847@qq.com>
2016-03-31 21:13:11 +0800

    update website

---
+++ js/load.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Administrator on 2016/2/5.
+ */
+window.onload=function(){
+
+    time();
+    back();
+    profile();
+    photo();
+    scroll();
+    works();
+    massage();
+    photoWall();
+    magnifying();
+    showPic();
+    fengqing();
+    phoneshow();
+    lagou();
+    calendar();
+    tabauto();
+    fllow();
+    photoList();
+    baiduSearch();
+	//nav();
+	
+    //var oUl=document.getElementById('ul_Wall');
+    //var aLi=oUl.children;
+    //var zIndex=99;
+    ////布局转换
+    //var aPos=[];
+    //for(var i=0;i<aLi.length;i++){
+    //    aPos.push({
+    //        left:aLi[i].offsetLeft,
+    //        top:aLi[i].offsetTop
+    //    });
+    //}
+    //
+    //for(var i=0;i<aLi.length;i++){
+    //    aLi[i].style.position='absolute';
+    //    aLi[i].style.left=aPos[i].left+'px';
+    //    aLi[i].style.top=aPos[i].top+'px';
+    //    aLi[i].style.margin=0+'px';
+    //}
+    //for(var i=0;i<aLi.length;i++){
+    //    aLi[i].index=i;
+    //    //drag(aLi[i]);
+    //}
+
+
+
+
+
+};
